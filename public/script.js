4a4a7175ed7c8eacbc9cb1014cbffd2ad1d7adbe
Lakshit Agarwal <115203789+LakshitAgarwal@users.noreply.github.com>
2023-07-08 20:26:46 +0530

    search box added

--- public/script.js
+++ public/script.js
@@ -36,3 +36,23 @@ window.addEventListener("scroll", handleScrollAnimation);
 
 // Trigger initial animation on page load
 window.addEventListener("load", handleScrollAnimation);
+
+
+const search = ()=>{
+  const searchbox = document.getElementById("search-item").value.toUpperCase();
+  const storeitems = document.getElementById("content");
+  const product = document.querySelectorAll(".card");
+  const pname = storeitems.getElementsByTagName("h4");
+
+  for(var i=0; i<pname.length; i++){
+    let match = product[i].getElementsByTagName('h4')[0];
+    if(match){
+      let textvalue = match.textContent || match.innerHTML;
+      if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+        product[i].style.display = "";
+      }else{
+        product[i].style.display = "none";
+      }
+    }
+  }
+}
