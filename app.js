b86774f72a9ef13ccf1a80a42d670d5f3a731ba8
Himesh Parashar <himeshparashar424@gmail.com>
2023-07-19 16:59:59 +0530

    search fixed
    first cached 20 pages on the server and then the search is working on the first 20 pages

--- app.js
+++ app.js
@@ -14,100 +14,130 @@ const app = express();
 const cache = new NodeCache();
 const scrapeInterval = 3600; // Cache expiration time in seconds
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+// Define an async function to fetch and cache data for all pages
+async function cacheAllPages() {
+  const totalPages = 20; // Adjust the total number of pages as needed
 
-// Route handler for the homepage
-app.get("/", async (req, res) => {
   try {
-    var page = req.query.page || 1;
-    // Fetch the links from the cache or scrape them if not available
-    let allLinks = cache.get("allLinks") ?? {};
-    // console.log(oldPage,page);
+    const fetchPagePromises = [];
 
-    var newLinks = [];
-    if (page in allLinks) {
-      newLinks = allLinks[page];
-    } else {
-      newLinks = await scrapPage(page);
-      allLinks[page] = newLinks;
-      cache.set("allLinks", allLinks, scrapeInterval);
-    }
-    currentLinks = newLinks;
-
-    // Render the EJS template with the links
-    res.render("index", { currentLinks, page });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-// Route handler for the course description page
-app.get("/course/:page/:h1", async (req, res) => {
-  try {
-    const h1 = req.params.h1;
-    const page = req.params.page;
-    // Fetch the links from the cache or scrape them if not available
-    let allLinks = cache.get("allLinks") ?? {};
-
-    if (page in allLinks) {
-      var links = allLinks[page];
-    } else {
-      links = await scrapPage(page);
-      allLinks[page] = links;
-      cache.set("allLinks", allLinks, scrapeInterval);
+    for (let page = 1; page <= totalPages; page++) {
+      fetchPagePromises.push(scrapPage(page));
     }
 
-    // Find the specific link based on the "h1" parameter
-    const course = links.find((course) => course.h1 === h1);
-    course.link = await scrapSingleCourse(course.courseLink);
+    const allLinksArray = await Promise.all(fetchPagePromises);
 
-    var randomPage = getRandomPropertyOfObject(allLinks);
-    var randomCourses = getRandomNThingsFromArray(allLinks[randomPage], 3);
-
-    console.log(course);
-    if (course) {
-      // Render the detailed course d escription page with the corresponding course data
-      res.render("course", { course, randomCourses, randomPage });
-    } else {
-      res.status(404).send("Course not found");
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-// Route handler for searchbox
-app.get("/search", async (req, res) => {
-  try {
-    const searchTerm = req.query.term; // Get the search term from the query parameter
-
-    // Fetch the links from the cache or scrape them if not available
-    const allLinks = cache.get("allLinks") ?? {};
-
-    const searchResults = [];
-    Object.values(allLinks).forEach((pageLinks) => {
-      const results = pageLinks.filter((link) =>
-        link.h1.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      searchResults.push(...results);
+    // Create a single object to store all links for each page
+    const allLinks = {};
+    allLinksArray.forEach((links, index) => {
+      allLinks[index + 1] = links;
     });
 
-    console.log(searchResults); // Print the searchResults to the console
+    // Cache all links with an expiration time (scrapeInterval) just like before
+    cache.set("allLinks", allLinks, scrapeInterval);
 
-    // Send a JSON response with the matching links
-    res.json({ searchResults });
+    console.log("All pages cached successfully!");
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error caching all pages:", error);
   }
-});
+}
 
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Call the function to cache all pages before starting the server
+cacheAllPages().then(() => {
+  // Set up view engine and static files
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "views"));
+  app.use(express.static("public"));
+
+  // Route handler for the homepage
+  app.get("/", async (req, res) => {
+    try {
+      var page = req.query.page || 1;
+      // Fetch the links from the cache or scrape them if not available
+      let allLinks = cache.get("allLinks") ?? {};
+
+      var newLinks = [];
+      if (page in allLinks) {
+        newLinks = allLinks[page];
+      } else {
+        newLinks = await scrapPage(page);
+        allLinks[page] = newLinks;
+        cache.set("allLinks", allLinks, scrapeInterval);
+      }
+      currentLinks = newLinks;
+
+      // Render the EJS template with the links
+      res.render("index", { currentLinks, page });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  // Route handler for the course description page
+  app.get("/course/:page/:h1", async (req, res) => {
+    try {
+      const h1 = req.params.h1;
+      const page = req.params.page;
+      // Fetch the links from the cache or scrape them if not available
+      let allLinks = cache.get("allLinks") ?? {};
+
+      if (page in allLinks) {
+        var links = allLinks[page];
+      } else {
+        links = await scrapPage(page);
+        allLinks[page] = links;
+        cache.set("allLinks", allLinks, scrapeInterval);
+      }
+
+      // Find the specific link based on the "h1" parameter
+      const course = links.find((course) => course.h1 === h1);
+      course.link = await scrapSingleCourse(course.courseLink);
+
+      var randomPage = getRandomPropertyOfObject(allLinks);
+      var randomCourses = getRandomNThingsFromArray(allLinks[randomPage], 3);
+
+      console.log(course);
+      if (course) {
+        // Render the detailed course description page with the corresponding course data
+        res.render("course", { course, randomCourses, randomPage });
+      } else {
+        res.status(404).send("Course not found");
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  // Route handler for searchbox
+  app.get("/search", async (req, res) => {
+    try {
+      const searchTerm = req.query.term; // Get the search term from the query parameter
+
+      // Fetch the links from the cache or scrape them if not available
+      const allLinks = cache.get("allLinks") ?? {};
+
+      const searchResults = [];
+      // Iterate through each page's links and filter based on the search term
+      Object.values(allLinks).forEach((pageLinks) => {
+        const results = pageLinks.filter((link) =>
+          link.h1.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        searchResults.push(...results);
+      });
+
+      // Send a JSON response with the matching links
+      res.json({ searchResults });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  // Start the server
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 });
--- scrape.js
+++ scrape.js
@@ -2,46 +2,59 @@ const { default: axios } = require("axios");
 const cheerio = require("cheerio");
 
 async function scrapPage(page) {
-    try {
-        const url = `https://www.discudemy.com/all/${page}`; // Update the URL here
-        const response = await axios.get(url);
-        var $ = cheerio.load(response.data);
-        const courseCards = $(".card").toArray();
-        let htmlCourseCards = courseCards.map(card => $.html(card));
+  try {
+    const url = `https://www.discudemy.com/all/${page}`; // Update the URL here
+    console.log("Fetching data from URL:", url);
 
-        var list = [];
-        htmlCourseCards.forEach(card => {
-            $ = cheerio.load(card);
-            var courseLink = $(".card-header").attr('href');
-            var h1 = $(".card-header").text();
-            var image = $("amp-img").attr('src')?.replace('240x135', '480x270');
-            var desc = $(".description").text().trim();
-            var price = $("span:nth-child(2):first").text().trim().replace('->$0','');
-            var c = { courseLink, h1, image, desc, price };
-            if(h1.length > 0){
-                list.push(c);   
-            }
+    const response = await axios.get(url);
+    console.log("Received response from URL:", url);
 
-        });
-        //   console.log(list);
-        return list;
+    var $ = cheerio.load(response.data);
+    const courseCards = $(".card").toArray();
+    console.log("Found", courseCards.length, "course cards on the page");
 
-    } catch (e) {
-        console.log(e)
-    }
+    let htmlCourseCards = courseCards.map((card) => $.html(card));
+    console.log("Converted course cards to HTML strings");
+
+    var list = [];
+    htmlCourseCards.forEach((card) => {
+      $ = cheerio.load(card);
+      var courseLink = $(".card-header").attr("href");
+      var h1 = $(".card-header").text();
+      var image = $("amp-img").attr("src")?.replace("240x135", "480x270");
+      var desc = $(".description").text().trim();
+      var price = $("span:nth-child(2):first")
+        .text()
+        .trim()
+        .replace("->$0", "");
+      var c = { courseLink, h1, image, desc, price };
+      if (h1.length > 0) {
+        list.push(c);
+      }
+    });
+
+    console.log("Extracted information for", list.length, "courses");
+    return list;
+  } catch (e) {
+    console.log("Error occurred during scraping:", e);
+  }
 }
-
 
 async function scrapSingleCourse(courseLink) {
-    var courseLinkRemove = courseLink.split("/");
-    courseLinkRemove[3] = 'go';
-    courseLink = courseLinkRemove.join("/");
+  var courseLinkRemove = courseLink.split("/");
+  courseLinkRemove[3] = "go";
+  courseLink = courseLinkRemove.join("/");
+  console.log("Modified courseLink:", courseLink);
 
-    const courseLinkResponse = await axios.get(courseLink);
-    const courseLinkData = courseLinkResponse.data;
-    const courseLinkPage = cheerio.load(courseLinkData);
-    return courseLinkPage("#couponLink").attr("href");
+  const courseLinkResponse = await axios.get(courseLink);
+  console.log("Received response from courseLink:", courseLink);
+
+  const courseLinkData = courseLinkResponse.data;
+  const courseLinkPage = cheerio.load(courseLinkData);
+  const couponLink = courseLinkPage("#couponLink").attr("href");
+  console.log("Extracted couponLink:", couponLink);
+
+  return couponLink;
 }
 
-
-module.exports = { scrapPage, scrapSingleCourse }
+module.exports = { scrapPage, scrapSingleCourse };
